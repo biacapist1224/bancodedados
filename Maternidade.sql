922ce66bc9739b9e0d2b492d0cacd5ef35c646ec
Beatriz capistrano de castro <119057340+biacapist1224@users.noreply.github.com>
2024-02-07 10:35:27 -0300

    Add files via upload

---
+++ Maternidade.sql
@@ -0,0 +1,23 @@
+insert into mae (nome)  value( Maria, júlia, Carla, Márcia, Giovana, Antonia, Francisca, Mônica, Lúcia, Vânia, Verônica, Marília, Kátia, Eliza, Vanessa);
+
+
+insert into mae (nome,endereco,cpf,telefone) values(1,Maria ,Rua 19,821.346.662-33,983645673,15-09-1999);
+insert into mae (nome,endereco,cpf,telefone) values(2,júlia,Rua 22,034.210.401-25,98974-8967,15-09-1981);
+insert into mae (nome,endereco,cpf,telefone) values(3,Carla,Rua 39,023.118.201-25,98827-5678,15-09-1982);
+insert into mae (nome,endereco,cpf,telefone) values(4,Márcia,Rua 47,081.221.550-45,98974-4367,15-10-1983);
+insert into mae (nome,endereco,cpf,telefone) values(5,Giovana,Rua 55,091.118.304-16,98756-7896,15-09-1984);
+
+insert into mae (nome,endereco,cpf,telefone) values(6,);
+insert into mae (nome,endereco,cpf,telefone) values(7,júlia,Rua 22,034.210.401-25,98974-8967,15-09-1981);
+insert into mae (nome,endereco,cpf,telefone) values(8,Carla,Rua 39,023.118.201-25,98827-5678,15-09-1982);
+insert into mae (nome,endereco,cpf,telefone) values(9,Márcia,Rua 47,081.221.550-45,98974-4367,15-10-1983);
+insert into mae (nome,endereco,cpf,telefone) values(10,Giovana,Rua 55,091.118.304-16,98756-7896,15-09-1984);
+insert into mae (nome,endereco,cpf,telefone) values(11,Maria ,Rua 19,821.346.662-33,983645673,15-09-1999);
+insert into mae (nome,endereco,cpf,telefone) values(12,júlia,Rua 22,034.210.401-25,98974-8967,15-09-1981);
+insert into mae (nome,endereco,cpf,telefone) values(13,Carla,Rua 39,023.118.201-25,98827-5678,15-09-1982);
+insert into mae (nome,endereco,cpf,telefone) values(14,Márcia,Rua 47,081.221.550-45,98974-4367,15-10-1983);
+insert into mae (nome,endereco,cpf,telefone) values(15,Giovana,Rua 55,091.118.304-16,98756-7896,15-09-1984);
+
+
+
+
